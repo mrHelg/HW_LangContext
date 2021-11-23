2b104b8598920bf89293d31fba8962f620b911c7
Elena <freshjs.ftm@gmail.com>
2021-11-23 09:56:39 +0200

    Initial commit UsersLoader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import React from 'react';
-import Ciao from './components/CiaoSection/Ciao';
-import Calendar from './components/Calendar';
-import FlexContainer from './components/FlexContainer';
+import UsersLoader from './components/UsersLoader';
+
 
 function App(){
   return <>
-    <FlexContainer jc='space-between' 
-    ai='center' fd='column' fw='wrap' 
-    title='@@@' style={{border:'1px solid red'}}
-    >
-      <Ciao id={2} name="Elon"/>
-      <Ciao id={3} name="Elen"/>
-      <Ciao id={4} name="Alex"/>
-    </FlexContainer>
-    {/* <Calendar /> */}
+  <UsersLoader />
   </>;
 }
 export default App;
---
+++ src/components/UsersLoader/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class UsersLoader extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
+UsersLoader.propTypes = {
+
+};
+
+
+export default UsersLoader;
