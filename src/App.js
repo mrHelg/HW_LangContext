adfc5b889edd42488fa8d1008b8939633ecbc995
Elena <freshjs.ftm@gmail.com>
2021-11-25 09:37:05 +0200

    Add BOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
-import UsersLoader from './components/UsersLoader';
+
 
 
 function App(){
   return <>
-  <UsersLoader />
+    <button onClick={()=>{window.close()}}>Close window</button>
+    <button onClick={()=>{window.open('http://localhost:3000/')}}>Open window</button>
+    <button onClick={()=>{window.location.assign('https://webpack.js.org/')}}>Open assign</button>
+    <button onClick={()=>{window.location.replace('https://webpack.js.org/')}}>Open replace</button>
   </>;
 }
 export default App;
