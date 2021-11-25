394c3b0aa13bf3809969207230aff57e9cdcae3e
Elena <freshjs.ftm@gmail.com>
2021-11-25 10:54:37 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import React from 'react';
 
 function App(){
   return <>
-    <button onClick={()=>{window.close()}}>Close window</button>
-    <button onClick={()=>{window.open('http://localhost:3000/')}}>Open window</button>
-    <button onClick={()=>{window.location.assign('https://webpack.js.org/')}}>Open assign</button>
-    <button onClick={()=>{window.location.replace('https://webpack.js.org/')}}>Open replace</button>
+    
   </>;
 }
 export default App;
