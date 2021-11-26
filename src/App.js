5f882f89c2140a05488c09a1aa80bf877bf3a9cd
Elena <freshjs.ftm@gmail.com>
2021-11-26 09:35:34 +0200

    Add 404

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet, useNavigate } from "react-router-dom";
 import Calendar from './components/Calendar';
 import SignInForm from './components/SignInForm';
-
+function Dashboard() {
+  return (
+    <div>
+      
+      <h1>Dashboard</h1>
+      <Outlet />
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
 function App(){
   return <>
   <BrowserRouter>
@@ -12,13 +21,19 @@ function App(){
         <li><Link to="/">Home</Link></li>
         <li><Link to="/calendar">Calendar</Link></li>
         <li><Link to="/sign-in">sign in</Link></li>
+        <li><Link to="/dash/messages">messages</Link></li>
+        <li><Link to="/dash/tasks">tasks</Link></li>
       </ul>
     </nav>
     <Routes>
-      <Route exact path="/" element={<Home />} >
+      <Route path="/" element={<Home />} />
+      <Route path="/calendar" element={<p>Calendar</p>} />
+      <Route path="/sign-in" element={<SignInForm />} />
+      <Route path="/dash/" element={<Dashboard />}>
+        <Route  path="messages" element={<p>messages</p>} />
+        <Route path="tasks" element={<p>tasks</p>} />
       </Route>
-      <Route exact path="/calendar" element={<Calendar />} ></Route>
-      <Route exact path="/sign-in" element={<SignInForm />} ></Route>
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
     <p>&copy;2021</p>
   </BrowserRouter>
@@ -26,6 +41,11 @@ function App(){
 }
 
 const Home = (props) =>{  return <h2>Home</h2>}
+const PageNotFound = (props) =>{ 
+    const navigate = useNavigate();
+    setTimeout(()=>{navigate(-1)}, 2000);
+    return <h2>404,  replace to home</h2>
+  }
 
 export default App;
 
